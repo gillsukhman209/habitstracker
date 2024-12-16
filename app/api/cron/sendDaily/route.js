26bc53219f96f2ee53a70d5c7c47b2d683263ee4
gillsukhman209 <104024595+gillsukhman209@users.noreply.github.com>
2024-12-16 13:12:58 -0800

    cron 7

--- app/api/cron/sendDaily/route.js
+++ app/api/cron/sendDaily/route.js
@@ -5,38 +5,43 @@ import User from "@/models/User";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function GET() {
-  try {
-    await connectMongo();
-    const users = await User.find({});
+  //   try {
+  console.log("Sending daily email");
+  return new Response(JSON.stringify({ message: "Email sent" }), {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
+  // await connectMongo();
+  // const users = await User.find({});
 
-    const BATCH_SIZE = 10;
-    const totalUsers = users.length;
+  // const BATCH_SIZE = 10;
+  // const totalUsers = users.length;
 
-    for (let i = 0; i < totalUsers; i += BATCH_SIZE) {
-      const batch = users.slice(i, i + BATCH_SIZE);
-      const emailPromises = batch.map((user) =>
-        resend.emails.send({
-          from: "21habits <onboarding@21habits.co>",
-          to: user.email,
-          subject: "21habits Reminder",
-          html: `<p>Hey ${user.email},<br>This is your daily habit reminder. Stay consistent!</p>`,
-        })
-      );
-      await Promise.all(emailPromises);
-    }
+  // for (let i = 0; i < totalUsers; i += BATCH_SIZE) {
+  //   const batch = users.slice(i, i + BATCH_SIZE);
+  //   const emailPromises = batch.map((user) =>
+  //     resend.emails.send({
+  //       from: "21habits <onboarding@21habits.co>",
+  //       to: user.email,
+  //       subject: "21habits Reminder",
+  //       html: `<p>Hey ${user.email},<br>This is your daily habit reminder. Stay consistent!</p>`,
+  //     })
+  //   );
+  //   await Promise.all(emailPromises);
+  // }
 
-    return new Response(
-      JSON.stringify({
-        success: true,
-        message: `Emails sent to ${totalUsers} users in batches.`,
-      }),
-      { status: 200, headers: { "Content-Type": "application/json" } }
-    );
-  } catch (error) {
-    console.error("Error sending email:", error);
-    return new Response(JSON.stringify({ error: "Failed to send email" }), {
-      status: 500,
-      headers: { "Content-Type": "application/json" },
-    });
-  }
+  // return new Response(
+  //   JSON.stringify({
+  //     success: true,
+  //     message: `Emails sent to ${totalUsers} users in batches.`,
+  //   }),
+  //   { status: 200, headers: { "Content-Type": "application/json" } }
+  // );
+  //   } catch (error) {
+  //     console.error("Error sending email:", error);
+  //     return new Response(JSON.stringify({ error: "Failed to send email" }), {
+  //       status: 500,
+  //       headers: { "Content-Type": "application/json" },
+  //     });
+  //   }
 }
