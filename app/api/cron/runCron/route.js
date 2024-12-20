a98fc19a1494ed2c1719e6ceb92400e191816e82
gillsukhman209 <104024595+gillsukhman209@users.noreply.github.com>
2024-12-20 14:56:02 -0800

    full url

--- app/api/cron/runCron/route.js
+++ app/api/cron/runCron/route.js
@@ -1,13 +1,16 @@
 export async function GET() {
   try {
+    console.log("Running cron job");
     // Make post request to sendDaily
-    await fetch("/api/cron/sendDaily", {
+    await fetch("https://21habits.co/api/cron/sendDaily", {
       method: "POST",
     });
+
+    console.log("Cron job run successfully");
 
     return new Response("Cron job run successfully", { status: 200 });
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
-    return new Response("Error running cron job", { status: 500 });
+    return new Response("Error running cron job", { status: 400 });
   }
 }
--- components/Habits.js
+++ components/Habits.js
@@ -100,7 +100,7 @@ function Habits({ habits, deleteHabit, onHabitsChange }) {
 
   const handleSendEmail = async () => {
     // api call to send email
-    await fetch("/api/cron/runCron");
+    await fetch("https://21habits.co/api/cron/runCron");
   };
 
   const handleResetHabits = async () => {
