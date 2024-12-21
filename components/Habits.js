4de6333d2ab68b0a960bdb65fed629e53ddfe0f0
gillsukhman209 <104024595+gillsukhman209@users.noreply.github.com>
2024-12-20 18:27:43 -0800

    route name update

--- components/Habits.js
+++ components/Habits.js
@@ -100,7 +100,7 @@ function Habits({ habits, deleteHabit, onHabitsChange }) {
 
   const handleSendEmail = async () => {
     // api call to send email
-    await fetch("/api/cron/runCron", {
+    await fetch("/api/cron/sendEmail", {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
