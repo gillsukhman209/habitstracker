aac4afa31738a5f9e18b4c1360ad6076ec2b90ba
gillsukhman209 <104024595+gillsukhman209@users.noreply.github.com>
2024-12-20 17:32:18 -0800

    cron job working, added resetAllHabits

--- app/api/cron/runCron/route.js
+++ app/api/cron/runCron/route.js
@@ -7,20 +7,34 @@ export const GET = cors(async (req) => {
     // Construct absolute URL using request headers
     const protocol = req.headers.get("x-forwarded-proto") || "http";
     const host = req.headers.get("host");
-    const url = `${protocol}://${host}/api/cron/sendDaily`;
+    const sendDailyUrl = `${protocol}://${host}/api/cron/sendDaily`;
+    const resetAllHabitsUrl = `${protocol}://${host}/api/cron/resetAllHabits`;
 
-    const res = await fetch(url, {
+    // Execute sendDaily cron job
+    const sendDailyRes = await fetch(sendDailyUrl, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
     });
 
-    if (!res.ok) {
+    if (!sendDailyRes.ok) {
       throw new Error("Failed to execute sendDaily cron job");
     }
 
-    return new Response("Cron job executed successfully.", { status: 200 });
+    // Execute resetAllHabits cron job
+    const resetAllHabitsRes = await fetch(resetAllHabitsUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!resetAllHabitsRes.ok) {
+      throw new Error("Failed to execute resetAllHabits cron job");
+    }
+
+    return new Response("Cron jobs executed successfully.", { status: 200 });
   } catch (error) {
     console.error("Error running cron job:", error);
     return new Response("Error running cron job", { status: 500 });
