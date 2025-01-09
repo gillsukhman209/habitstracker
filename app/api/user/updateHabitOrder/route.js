be42f17dda8a7bd66ab1e50b2b19cc82c6e0cbfc
gillsukhman209 <104024595+gillsukhman209@users.noreply.github.com>
2025-01-08 21:53:05 -0800

    added updateHabitOrder file

---
+++ app/api/user/updateHabitOrder/route.js
@@ -0,0 +1,26 @@
+import connectMongo from "@/libs/mongoose";
+import User from "@/models/User";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/libs/next-auth";
+
+export async function POST(req) {
+  await connectMongo();
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+
+  const { habits } = await req.json();
+
+  const user = await User.findById(session.user.id);
+  user.habits = habits.map((habit, index) => ({
+    ...habit,
+    order: index,
+  }));
+
+  await user.save();
+  return new Response(JSON.stringify({ message: "Order updated" }), {
+    status: 200,
+  });
+}
