5256329fa9160d4cb0027322f1c85c9dd420a169
gillsukhman209 <104024595+gillsukhman209@users.noreply.github.com>
2025-01-09 18:09:01 -0800

    changed ui of rebrand

--- app/dashboard/dashboardClient.jsx
+++ app/dashboard/dashboardClient.jsx
@@ -183,12 +183,14 @@ export default function Dashboard() {
         />
 
         {showPopup && (
-          <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex items-center justify-center">
-            <div className="bg-base-100 p-6 rounded-md shadow-lg w-96 transform scale-105 transition-all duration-300">
-              <h2 className="text-xl font-bold mb-4">Add New Habits</h2>
+          <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex items-center justify-center ">
+            <div className="bg-base-100 p-8 rounded-xl shadow-xl w-96 transform scale-100 transition-all duration-300">
+              <h2 className="text-2xl font-bold text-primary mb-6 text-center">
+                Add New Habit
+              </h2>
 
               {habitInputs.map((input, index) => (
-                <div key={index} className="mb-4">
+                <div key={index} className="mb-6">
                   <input
                     type="text"
                     placeholder="Habit Title"
@@ -196,8 +198,8 @@ export default function Dashboard() {
                     onChange={(e) =>
                       handleInputChange(index, "title", e.target.value)
                     }
-                    className="input input-bordered input-info w-full max-w-xs mb-2"
-                    onKeyDown={handleKeyDown} // Add key down event
+                    className="input input-bordered input-primary w-full mb-4 text-sm"
+                    onKeyDown={handleKeyDown}
                   />
 
                   {input.toggleOption === "count" ? (
@@ -208,8 +210,8 @@ export default function Dashboard() {
                       onChange={(e) =>
                         handleInputChange(index, "count", e.target.value)
                       }
-                      className="input input-bordered input-info w-full max-w-xs mb-2"
-                      onKeyDown={handleKeyDown} // Add key down event
+                      className="input input-bordered input-primary w-full mb-4 text-sm"
+                      onKeyDown={handleKeyDown}
                     />
                   ) : (
                     <input
@@ -219,63 +221,63 @@ export default function Dashboard() {
                       onChange={(e) =>
                         handleInputChange(index, "duration", e.target.value)
                       }
-                      className="input input-bordered input-info w-full max-w-xs mb-2"
-                      onKeyDown={handleKeyDown} // Add key down event
+                      className="input input-bordered input-primary w-full mb-4 text-sm"
+                      onKeyDown={handleKeyDown}
                     />
                   )}
 
-                  <div className="flex items-center justify-around w-full ">
-                    <span className="mr-2 text-base-content">Count</span>
+                  <div className="flex items-center justify-between w-full text-sm">
+                    <span className="text-base-content">Count</span>
                     <label className="relative inline-flex items-center cursor-pointer">
                       <input
                         type="checkbox"
                         className="sr-only"
                         onChange={() => handleToggleChange(index)}
                       />
-                      <div className="w-10 h-6 bg-gray-700 rounded-full shadow-inner"></div>
-                      <div
-                        className={`dot absolute w-4 h-4 bg-white rounded-full shadow transition-transform duration-200 ease-in-out ${
-                          input.toggleOption === "count"
-                            ? "translate-x-0"
-                            : "translate-x-4"
-                        }`}
-                      ></div>
+                      <div className="w-12 h-6 bg-gray-300 rounded-full shadow-inner transition-colors">
+                        <div
+                          className={`dot absolute w-6 h-6 bg-primary rounded-full shadow transition-transform duration-200 ease-in-out ${
+                            input.toggleOption === "count"
+                              ? "translate-x-0"
+                              : "translate-x-6"
+                          }`}
+                        ></div>
+                      </div>
                     </label>
-                    <span className="ml-2 text-base-content">Duration</span>
+                    <span className="text-base-content">Duration</span>
                   </div>
 
-                  {/* New Checkbox for getCharged */}
-                  <div className="flex items-center mt-2">
+                  <div className="flex items-center mt-4 text-sm">
                     <input
                       type="checkbox"
-                      checked={input.getCharged} // Checkbox should be checked by default
+                      checked={input.getCharged}
                       onChange={(e) =>
                         handleInputChange(index, "getCharged", e.target.checked)
                       }
-                      className="mr-2"
+                      className="checkbox checkbox-primary mr-2"
                     />
                     <label className="text-base-content">Get Charged</label>
                   </div>
                 </div>
               ))}
 
-              <div className="flex justify-between mt-4 items-center">
+              <div className="flex justify-between mt-6">
                 <button
-                  className="px-4 py-2 bg-gray-300 text-gray-900 rounded-md"
+                  className="px-4 py-2 bg-gray-200 text-gray-800 rounded-md hover:bg-gray-300 transition-all"
                   onClick={() => setShowPopup(false)}
                 >
                   Cancel
                 </button>
 
                 <button
-                  className="px-4 py-2 bg-gray-300 text-gray-900 rounded-md"
+                  className="px-4 py-2 bg-gray-200 text-gray-800 rounded-md hover:bg-gray-300 transition-all"
                   onClick={addMoreHabitFields}
                 >
                   Add More
                 </button>
 
                 <button
-                  className="px-4 py-2 bg-blue-500 text-white rounded-md"
+                  className="px-4 py-2 bg-primary text-white rounded-md hover:bg-blue-700 transition-all"
                   onClick={addHabits}
                 >
                   Save
